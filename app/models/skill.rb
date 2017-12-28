a5bf96a482df188d477622f8c8656ba99ce2b754
Hector Sanchez <sanchez.hector.m@gmail.com>
2017-12-28 11:30:35 -0500

    Integrated technologies model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,7 +3,7 @@ class Skill < ApplicationRecord
 
   validates_presence_of :title, :percent_utilized
 
-  after_initialized :set_defaults
+  after_initialize :set_defaults
 
   def set_defaults
     self.badge ||= Placeholder.image_generator(height: '250', width: '250')
