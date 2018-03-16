54807c5abad06fa9c38cb99ab2a5b4b1f4004445
Hector Sanchez <sanchez.hector.m@gmail.com>
2018-03-16 00:43:12 -0400

    Updated portfolio to include attr for position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,6 +8,10 @@ class Portfolio < ApplicationRecord
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  def self.by_position
+    order("position ASC")
+  end
+
   def self.angular
     where(subtitle: 'Angular')
   end
